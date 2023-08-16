78c54ee4842ffe79b67d9e24672887d37c9d045d
StoyanKyopchev <Stoyank43@gmail.com>
2023-08-15 19:57:05 -0700

    CSS responsiveness optimization for laptop screen sizes

--- src/Components/Main/carousel/Carousel.jsx
+++ src/Components/Main/carousel/Carousel.jsx
@@ -33,6 +33,13 @@ export default function Carousel() {
         slidesToScroll: 5,
         responsive: [
           {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+          },
+          {
             breakpoint: 480,
             settings: {
               slidesToShow: 1,
