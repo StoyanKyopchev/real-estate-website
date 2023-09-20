12eed72044c3f54e47a361075e1d94b28ea65b0c
StoyanKyopchev <stoyank43@gmail.com>
2023-09-20 12:13:33 +0300

    Moved the React Router setup to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./Contexts/AuthContext";
 import { useTranslation } from "react-i18next";
 import Hero from "./Components/Hero/Hero.jsx";
 import Main from "./Components/Main/Main.jsx";
 import ChooseUs from "./Components/ChooseUs/ChooseUs.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
+import AccDashboard from "./Components/Hero/AuthModal/AccDashboard.jsx";
+import SignIn from "./Components/Hero/AuthModal/SignIn.jsx";
+import SignUp from "./Components/Hero/AuthModal/SignUp.jsx";
+import PrivateRoute from "./Components/Hero/AuthModal/PrivateRoute.jsx";
+import PasswordReset from "./Components/Hero/AuthModal/PasswordReset.jsx";
+import AccUpdate from "./Components/Hero/AuthModal/AccUpdate.jsx";
 import "./CSS/global.css";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
@@ -11,16 +19,64 @@ import "slick-carousel/slick/slick-theme.css";
 export const TranslatorContext = React.createContext();
 
 export default function App() {
+  const [openModal, setOpenModal] = useState(false);
   const { t, i18n } = useTranslation();
 
   return (
     <>
-      <TranslatorContext.Provider value={{ t, i18n }}>
-        <Hero />
-        <Main />
-        <ChooseUs />
-        <Footer />
-      </TranslatorContext.Provider>
+      <AuthProvider>
+        <TranslatorContext.Provider value={{ t, i18n }}>
+          <Routes>
+              <Route 
+                  exact path="/"
+                  element={
+                          <PrivateRoute>
+                              <AccDashboard 
+                                  isOpen={openModal} 
+                                  onClose={() => setOpenModal(false)}
+                              />
+                          </PrivateRoute>
+                  }
+              />
+              <Route 
+                  path="/signup" 
+                  element={<SignUp 
+                              isOpen={openModal} 
+                              onClose={() => setOpenModal(false)} 
+                          />} 
+              />
+              <Route 
+                  path="/signin" 
+                  element={<SignIn 
+                              isOpen={openModal} 
+                              onClose={() => setOpenModal(false)} 
+                          />} 
+              />
+              <Route
+                  path="/passwordreset"
+                  element={<PasswordReset
+                              isOpen={openModal} 
+                              onClose={() => setOpenModal(false)}  
+                          />}
+              />
+              <Route 
+                  path="/accountupdate"
+                  element={
+                          <PrivateRoute>
+                              <AccUpdate
+                                  isOpen={openModal}
+                                  onClose={() => setOpenModal(false)}
+                              />
+                          </PrivateRoute>
+                  }
+              />
+          </Routes>
+          <Hero openModal={openModal} setOpenModal={setOpenModal} />
+          <Main />
+          <ChooseUs />
+          <Footer />
+        </TranslatorContext.Provider>
+      </AuthProvider>
     </>
   );
 }
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,18 +1,9 @@
 import { useState, useEffect, useContext } from "react";
 import { TranslatorContext } from "../../App";
-import { AuthProvider } from "../../Contexts/AuthContext";
-import { Routes, Route } from "react-router-dom";
 import Navigation from "./Navigation/Navigation";
-import AccDashboard from "./AuthModal/AccDashboard";
-import SignIn from "./AuthModal/SignIn";
-import SignUp from "./AuthModal/SignUp";
-import PrivateRoute from "./AuthModal/PrivateRoute";
-import PasswordReset from "./AuthModal/PasswordReset";
-import AccUpdate from "./AuthModal/AccUpdate";
 import "./hero.css"
 
-export default function Hero() {
-    const [openModal, setOpenModal] = useState(false);
+export default function Hero({ openModal, setOpenModal }) {
     const [languageMenuOpen, setLanguageMenuOpen] = useState(false);
     const { t } = useContext(TranslatorContext);
 
@@ -37,82 +28,35 @@ export default function Hero() {
 
     return (
         <>
-            <AuthProvider>
-                <Routes>
-                    <Route 
-                        exact path="/"
-                        element={
-                                <PrivateRoute>
-                                    <AccDashboard 
-                                        isOpen={openModal} 
-                                        onClose={() => setOpenModal(false)}
-                                    />
-                                </PrivateRoute>
-                        }
-                    />
-                    <Route 
-                        path="/signup" 
-                        element={<SignUp 
-                                    isOpen={openModal} 
-                                    onClose={() => setOpenModal(false)} 
-                                />} 
-                    />
-                    <Route 
-                        path="/signin" 
-                        element={<SignIn 
-                                    isOpen={openModal} 
-                                    onClose={() => setOpenModal(false)} 
-                                />} 
-                    />
-                    <Route
-                        path="/passwordreset"
-                        element={<PasswordReset
-                                    isOpen={openModal} 
-                                    onClose={() => setOpenModal(false)}  
-                                />}
-                    />
-                    <Route 
-                        path="/accountupdate"
-                        element={
-                                <PrivateRoute>
-                                    <AccUpdate
-                                        isOpen={openModal}
-                                        onClose={() => setOpenModal(false)}
-                                    />
-                                </PrivateRoute>
-                        }
-                    />
-                </Routes>
-                <div 
-                    className="heroWrapper" 
-                    onClick={() => setLanguageMenuOpen(false)}
-                >
-                    <Navigation 
-                        setOpenModal={setOpenModal} 
-                        toggleLanguageDropdown={toggleLanguageDropdown}
-                        languageMenuOpen={languageMenuOpen}
-                    />
-                    <div className="innerHeroWrapper">
-                        <div className="headingWrapper">
-                            <h1>{t("Hero.h1")}</h1>
-                        </div>
-                        <div className="searchBarOuterWrapper">
-                            <div className="searchBarWrapper">
-                                <input 
-                                    type="text" 
-                                    placeholder={t("Hero.placeholder")}
-                                    className="searchBar" 
-                                />
-                                <span className="searchButtonWrapper">
-                                    <button>
-                                        <img src={require("../../images/heroSectionImages/searchButton.png")} />
-                                    </button>
-                                </span>
-                            </div>
+            <div 
+                className="heroWrapper" 
+                onClick={() => setLanguageMenuOpen(false)}
+            >
+                <Navigation 
+                    setOpenModal={setOpenModal} 
+                    toggleLanguageDropdown={toggleLanguageDropdown}
+                    languageMenuOpen={languageMenuOpen}
+                />
+                <div className="innerHeroWrapper">
+                    <div className="headingWrapper">
+                        <h1>{t("Hero.h1")}</h1>
+                    </div>
+                    <div className="searchBarOuterWrapper">
+                        <div className="searchBarWrapper">
+                            <input 
+                                type="text" 
+                                placeholder={t("Hero.placeholder")}
+                                className="searchBar" 
+                            />
+                            <span className="searchButtonWrapper">
+                                <button>
+                                    <img src={require("../../images/heroSectionImages/searchButton.png")} />
+                                </button>
+                            </span>
                         </div>
                     </div>
                 </div>
-            </AuthProvider>
+            </div>
         </>
     )
 }
